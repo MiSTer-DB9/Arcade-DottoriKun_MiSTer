81bc91f873ec000cd365b7ffbf6688d08901b8f2
Timothy Redaelli <timothy.redaelli@gmail.com>
2026-06-09 15:34:36 +0200

    DB9: consume programmable remap matrix at joystick merge

--- Arcade-DottoriKun.sv
+++ Arcade-DottoriKun.sv
@@ -338,7 +338,7 @@ wire        ioctl_wait;
 wire [15:0] joystick_0_USB, joystick_1_USB;
 // [MiSTer-DB9 END]
 // [MiSTer-DB9-Pro BEGIN] - DB controllers muted while OSD is open (1P: only joystick_0 muxed)
-wire [31:0] joystick_0 = joydb_1ena ? (OSD_STATUS ? 32'b0 : {joydb_1[11],joydb_1[10],joydb_1[5:0]}) : joystick_0_USB;
+wire [31:0] joystick_0 = joydb_1ena ? (OSD_STATUS ? 32'b0 : joydb_1_mapped[7:0]) : joystick_0_USB;
 wire [15:0] joystick_1 = joystick_1_USB;
 // [MiSTer-DB9-Pro END]
 wire [15:0] joy = joystick_0 | joystick_1;
